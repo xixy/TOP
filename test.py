d8aeaff52c9cef40fbe4f8942933a75a06f1f953
xixy10 <xixy10@foxmail.com>
2017-07-27 18:51:42 +0800

    修改了login

--- test.py
+++ test.py
@@ -22,46 +22,15 @@ from answerDAO import answerDAO
 from answer import answer
 import configure
 
-
-import time
- 
-#产生子进程，而后父进程退出
-pid = os.fork()
-if pid > 0:
-    sys.exit(0)
- 
-#修改子进程工作目录
-os.chdir("/")
-#创建新的会话，子进程成为会话的首进程
-os.setsid()
-#修改工作目录的umask
-os.umask(0)
- 
-#创建孙子进程，而后子进程退出
-pid = os.fork()
-if pid > 0:
-    sys.exit(0)
- 
-#重定向标准输入流、标准输出流、标准错误
-sys.stdout.flush()
-sys.stderr.flush()
-si = file("/dev/null", 'r')
-so = file("/dev/null", 'a+')
-se = file("/dev/null", 'a+', 0)
-os.dup2(si.fileno(), sys.stdin.fileno())
-os.dup2(so.fileno(), sys.stdout.fileno())
-os.dup2(se.fileno(), sys.stderr.fileno())
-
-
-
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 CORS(app, supports_credentials=True)
 
 @app.route('/login',methods=['POST'])
 def login():
-    username=request.data[username]
-    password=request.data[password]
+    data=request.get_json()
+    username=data[configure.student_name]
+    password=data[configure.student_student_password]
     result=studentInfoDAO.valid(username,password)
     return jsonify({"id":result}),200
 
